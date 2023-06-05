27a6a597c80ed21db1ad7a4fe020844a59122e79
aniljos <anil.jos@gmail.com>
2023-06-05 10:11:47 +0530

    Day 1 Session 1

---
+++ Typescript/build/hello.js
@@ -0,0 +1,9 @@
+console.log("Hello adasads");
+var x; // implicily type is any
+x = 10;
+x = 'abc';
+var y; // Type annotation==> type is a number
+y = 100;
+//y  = "abc"; // static type checking
+var z = 20; // Type inference ==> type is a number
+//z = "adsasd";
